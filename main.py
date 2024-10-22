8c0b24acc7ca86de06e6bd58f6d810c617836957
PradepKaur <6250pradeepkaur@gmail.com>
2024-10-22 10:22:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import os
 import json
+import gdown
 from PIL import Image
 import numpy as np
 import tensorflow as tf
 import streamlit as st
 
+# Set up working directory and paths
 working_dir = os.path.dirname(os.path.abspath(__file__))
-model_path = f"{working_dir}/trained_model/tomatoes_disease_prediction_model.h5"
+model_file = 'tomatoes_disease_prediction_model.h5'
+model_path = f"{working_dir}/{model_file}"
+class_indices_path = f"{working_dir}/class_indices.json"
+
+# Google Drive file ID for the model
+file_id = "1-71_uDEa0URjFg7V4QmSP4vvVqRIOX4N"  # Replace with your actual file ID
+url = f"https://drive.google.com/uc?id={file_id}"
+
+# Check if the model already exists locally; if not, download it
+if not os.path.exists(model_path):
+    st.write("Downloading the model from Google Drive...")
+    gdown.download(url, model_path, quiet=False)
+
 # Load the pretrained model
 model = tf.keras.models.load_model(model_path)
 
-class_indices = json.load(open(f"{working_dir}/class_indices.json"))
+# Load the class indices
+with open(class_indices_path, 'r') as f:
+    class_indices = json.load(f)
+
 
 # Function to Load and Preprocess the Image using Pillow
 def load_and_preprocess_image(image_path, target_size=(224, 224)):
@@ -63,4 +80,3 @@ if uploaded_image is not None:
             # Preprocess the uploaded image and predict the class
             prediction = predict_image_class(model, uploaded_image, class_indices)
             st.success(f'Prediction: {str(prediction)}')
-
